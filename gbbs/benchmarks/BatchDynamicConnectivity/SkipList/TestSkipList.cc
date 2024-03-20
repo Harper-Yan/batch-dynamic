e0a4c6c7becfb1bcede691a842c08fdced950041
Sean Chester <schester@uvic.ca>
2024-03-20 11:42:55 -0700

    Add two hello world test cases to SkipList unit tests

--- gbbs/benchmarks/BatchDynamicConnectivity/SkipList/TestSkipList.cc
+++ gbbs/benchmarks/BatchDynamicConnectivity/SkipList/TestSkipList.cc
@@ -3,15 +3,19 @@
 #include "benchmarks/BatchDynamicConnectivity/SkipList/SkipList.h"
 #include "single_include/catch2/catch.hpp"
 
+TEST_CASE( "Equality operator == matches two empty skiplists", "[skiplist][equality]" )
+{
+    gbbs::SkipList default_constructed_list;
+    gbbs::SkipList zero_initialised_list(0);
 
-unsigned int Factorial( unsigned int number ) {
-    return number <= 1 ? number : Factorial(number-1)*number;
+    REQUIRE( default_constructed_list == zero_initialised_list );
 }
 
-TEST_CASE( "Factorials are computed", "[factorial]" ) {
-    REQUIRE( Factorial(0) == 1 );
-    REQUIRE( Factorial(1) == 1 );
-    REQUIRE( Factorial(2) == 2 );
-    REQUIRE( Factorial(3) == 6 );
-    REQUIRE( Factorial(10) == 3628800 );
+TEST_CASE( "Equality operator == failes with just one empty skiplist", "[skiplist][equality]" )
+{
+    gbbs::SkipList default_constructed_list;
+    gbbs::SkipList zero_initialised_list(42);
+
+    REQUIRE( default_constructed_list == zero_initialised_list );
 }
+
